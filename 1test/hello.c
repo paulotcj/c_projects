31afab16e56d1cf1f7b0c23cfb0d557497a2f3f4
Paulo <paulotcj@gmail.com>
2022-06-26 14:53:46 -0400

    .

--- 1test/hello.c
+++ 1test/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define DEFINE_NUMBER_HEADER 111
  
 int main(int argc, char *argv[])
 {
@@ -142,8 +146,118 @@ int main(int argc, char *argv[])
 
     binary3 = binary3 << 1; //111 << 1 = 1110 => 14
 
-    printf("binary3 : %i\n", binary3);   
+    printf("binary3 : %i\n", binary3);
 
+    printf("-------------------------------------\n");
+    //BIT CHECK
+    // the idea here is you get an integer which you either have set in decimal form or binary
+    // and then you want to check whether a specific bit is set
+    // Most commonly used when you want to set flags for a certain operation, as in:
+    // 0bXXXX
+    //   ||||-> do action 1
+    //   |||--> do action 2
+    //   ||---> do action 3
+    //   |----> do action 4
+
+    //We have the example below:
+    binary1 = 0b00010001;
+    binary2 = 0b00010000;   
+
+    binary3 = binary1 & binary2;
+
+    printf("%i\n",binary3);
+
+    // in the case above we can see both numbers have 1 set in the 4th position
+    // therefore the bit operator AND (&) should return '00010000' => 16.
+    // if you use this in an IF statement, it will result in a true condition.
+
+    //Now with this case:
+    binary1 = 0b00010001;
+    binary2 = 0b00001000;   
+
+    binary3 = binary1 & binary2;
+
+    printf("%i\n",binary3);
+    // we should have zero as result, since none of the bits set ( to 1) would match
+    printf("-------------------------------------\n");
+
+    int num1 = 10, num2 = 3;
+    float num3 = num1/num2; //we lose the decimal values
+    printf("num3: %f\n",num3);
+
+    num3 = (float)(num1/num2);//no effect
+    printf("num3: %f\n",num3);
+
+        num3 = (float)num1/num2;//it works now
+    printf("num3: %f\n",num3);
+
+    printf("-------------------------------------\n");
+
+    printf("sizeof(num1) : %lu\n",sizeof(num1));
+    printf("sizeof(num3) : %lu\n",sizeof(num3));
+
+    int int_array1[10];
+
+    printf("sizeof(int_array1) with 10 positions : %lu\n",sizeof(int_array1));
+
+    printf("int_array1 count : %lu\n", 
+        sizeof(int_array1)/sizeof(int_array1[0])
+    );
+
+    printf("-------------------------------------\n");
+
+    //ternary operators
+
+    num3 = num1 == num2 ? 1111 : 2222 ;
+
+    printf("num3: %f\n",num3);
+
+    printf("-------------------------------------\n");
+
+    printf("DEFINE_NUMBER_HEADER: %d\n",DEFINE_NUMBER_HEADER);
+
+    printf("-------------------------------------\n");
+
+    time_t time1;
+    time(&time1);
+
+    printf("time1 = %ld\n", time1);
+
+
+    struct tm * timeinfo;
+
+    timeinfo = localtime ( &time1 );
+    printf ( "Current local time and date: %s", asctime (timeinfo) );
+
+    //initialize the random number generator
+    srand(time1);
+
+    //get random number
+    // num1 = rand();
+    for(int i = 0 ; i < 10 ; i++) { printf("rand(): %d\n",rand()); }
+    
+    printf("-------------------------------------\n");
+    //arrays
+
+    int int_arr1[10];
+    for(int i = 0 ; i < 10 ; i++) { printf("int_arr1[%d]: %d\n", i , int_arr1[i]); }
+
+    int int_arr2[10] = {}; //initializes all positions with 0
+    for(int i = 0 ; i < 10 ; i++) { printf("int_arr2[%d]: %d\n", i , int_arr2[i]); }
+
+    int int_arr3[3][4] = 
+    {
+        {1,2,3,4},
+        {10,20,30,40},
+        {100,200,300,400}
+    };
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        for(int j = 0 ; j < 4 ; j++)
+        {
+            printf("int_arr3[%d][%d]: %d\n", i, j , int_arr3[i][j]);
+        }
+    }
 
     return 0;
 }
